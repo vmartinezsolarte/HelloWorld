551f9718033f8ec8df13a02f4fbf13983f761e47
vmartinezsolarte <vmartinezsolarte@gmail.com>
2019-05-28 21:07:47 -0500

    new class for handling the printing stuff

--- hello.java
+++ hello.java
@@ -1,11 +1,14 @@
-//import java.utils.*;
+
+//package HelloWorld;
+//import print2;
+
 
 
 public class hello{
 
   public static void main(String [] args){
-  
-  System.out.println("Hello World!");
-  }
+  print2 pr = new print2();
+  pr.greetings(" You!");  
+}
 
 }
